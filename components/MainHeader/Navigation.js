3c6104d75867a5c971b4b781ce957a034c6e88e8
PoonamRai23 <128670417+PoonamRai23@users.noreply.github.com>
2024-01-18 21:01:38 +0530

    Initialize project using Create React App

---
+++ components/MainHeader/Navigation.js
@@ -0,0 +1,34 @@
+import React,{useContext} from 'react';
+
+import React, { useContext } from 'react';
+
+import AuthContext from '../../store/auth-context';
+import classes from './Navigation.module.css';
+import AuthContext from '../../store/AuthContext';
+
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
